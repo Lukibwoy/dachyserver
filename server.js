d0c55455992c4970d04e084700309709b8feca98
Łukasz <86016888+Lukibwoy@users.noreply.github.com>
2025-01-15 18:30:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path')
 const fs = require('fs')
 
 const app = express()
-const PORT = 5000
+const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(bodyParser.json())
